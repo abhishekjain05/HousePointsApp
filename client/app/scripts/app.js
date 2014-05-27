2f1a538730ff8b2ae27504a1ae801280870d59b0
jain208 <abhi2000@gmail.com>
2014-05-27 15:06:22 +0100

    added skeleton pages for register and login, plus changed ui-router routes config

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -2,8 +2,24 @@
 
 angular
   .module('clientApp', ['ui.router'])
+  .run(function($rootScope){
+
+    $rootScope.$on('$stateChangeStart', function (ev, to, toParams, from, fromParams) {
+      /*if (!routeClean($location.url()) && !AuthenticationService.isLoggedIn()) {
+        // redirect back to login
+        $location.path('/login');
+      }
+      else if (routeAdmin($location.url() && !UserService.validateRoleAdmin()) {
+        // redirect to error page
+        $location.path('/error');
+      }*/
+      console.log(to);
+    });
+     
+  })
   .config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.when("/", "/notes");
+    $urlRouterProvider.otherwise("/notes");
 
     $stateProvider
       .state('home', {
@@ -11,12 +27,20 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .state('notes', {
-        url: '/notes',
+      .state('home.notes', {
+        url: 'notes',
         templateUrl: 'views/notes.html',
         controller: 'NotesCtrl'
       })
-      .state('notes.category', {
+      .state('home.register', {
+        url: 'register',
+        templateUrl: 'views/register.html'
+      })      
+      .state('home.login', {
+        url: 'login',
+        templateUrl: 'views/login.html'
+      })      
+      .state('home.notes.category', {
         url: '/:category',
         templateUrl: 'views/category.html',
         controller: 'NotesCategoryController'
